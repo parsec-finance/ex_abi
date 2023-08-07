e0a850cffc4f1c5c22f7c6d6303924a865820869
sheehan1102 <sheehan1102@gmail.com>
2023-08-07 13:21:34 -0700

    Update function_selector.ex

--- lib/abi/function_selector.ex
+++ lib/abi/function_selector.ex
@@ -400,7 +400,7 @@ defmodule ABI.FunctionSelector do
   defp add_method_id(selector) do
     signature = encode(selector)
 
-    case ExKeccak.hash_256(signature) do
+    case :keccakf1600.sha3_256(signature) do
       <<method_id::binary-size(4), _::binary>> ->
         %{selector | method_id: method_id}
 
